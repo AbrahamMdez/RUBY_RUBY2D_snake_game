6ef084c4d2f73fc903fe50fec2480d163fc31cfc
Abraham <nencho_1@hotmail.com>
2021-04-20 13:57:19 +0200

    created logic to validate o invalidate when you try to move snake to some place

--- test/actions_test.rb
+++ test/actions_test.rb
@@ -2,31 +2,65 @@ require_relative '../src/actions/actions'
 require_relative '../src/model/state'
 
 class ActionsTest 
+
+    def setup
+        @initial_state = Model::State.new(
+            Model::Food.new( 4, 4),
+            Model::Snake.new([
+                Model::Coord.new( 1, 1),
+                Model::Coord.new( 0, 1)
+            ]),
+            Model::Grid.new( 8, 12),
+            Model::Direction::DOWN,
+            false
+        )
+    end
+
     def test_move_snake
-        initial_state = Model::State.new(
-                            Model::Food.new( 4, 4),
-                            Model::Snake.new([
-                                Model::Coord.new( 1, 1),
-                                Model::Coord.new( 0, 1)
-                            ]),
-                            Model::Grid.new( 8, 12),
-                            Model::Direction::DOWN,
-                            false
+        expect_state =  Model::State.new(
+            Model::Food.new( 4, 4),
+            Model::Snake.new([
+                Model::Coord.new( 1, 1),
+                Model::Coord.new( 0, 1)
+            ]),
+            Model::Grid.new( 8, 12),
+            Model::Direction::DOWN,
+            false
         )
 
-        expect_state = initial_state = 
-            Model::State.new(
-                Model::Food.new( 4, 4),
-                Model::Snake.new([
-                    Model::Coord.new( 1, 1),
-                    Model::Coord.new( 0, 1)
-                ]),
-                Model::Grid.new( 8, 12),
-                Model::Direction::DOWN,
-                false
+        actual_state = Actions::move_snake(@initial_state)
+        assert_equal actual_state, expect_state
+    end
+
+    def test_change_direction_invalid
+        expect_state =  Model::State.new(
+            Model::Food.new( 4, 4),
+            Model::Snake.new([
+                Model::Coord.new( 1, 1),
+                Model::Coord.new( 0, 1)
+            ]),
+            Model::Grid.new( 8, 12),
+            Model::Direction::DOWN,
+            false
         )
 
-        actual_state = Actions::move_snake(initial_state)
+        actual_state = Actions::change_direction(@initial_state, Model::Direction::UP)
+        assert_equal actual_state, expect_state
+    end
+
+    def test_change_direction_valid
+        expect_state =  Model::State.new(
+            Model::Food.new( 4, 4),
+            Model::Snake.new([
+                Model::Coord.new( 1, 1),
+                Model::Coord.new( 0, 1)
+            ]),
+            Model::Grid.new( 8, 12),
+            Model::Direction::LEFT,
+            false
+        )
+
+        actual_state = Actions::change_direction(@initial_state, Model::Direction::LEFT)
         assert_equal actual_state, expect_state
     end
 end
