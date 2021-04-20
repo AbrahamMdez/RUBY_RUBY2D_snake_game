32eb5d3d77963d03fb08e71adfe1187c9d005265
Abraham <nencho_1@hotmail.com>
2021-04-20 10:03:18 +0200

    did all movements to the snake

--- src/model/state.rb
+++ src/model/state.rb
@@ -19,7 +19,7 @@ module Model
     class Grid < Struct.new(:rows, :cols)
     end
 
-    class State < Struct.new(:food, :snake, :grid, :next_direction)
+    class State < Struct.new(:food, :snake, :grid, :next_direction, :game_finished)
     end
 
     def self.initial_state
@@ -30,7 +30,8 @@ module Model
                 Model::Coord.new( 0, 1)
             ]),
             Model::Grid.new( 8, 12),
-            Direction::DOWN
+            Direction::DOWN,
+            false
         )
     end
 end
