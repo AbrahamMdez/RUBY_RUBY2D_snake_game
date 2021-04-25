18b8f7a650465c29c0f1ba68e3405ea19adc1895
Abraham <nencho_1@hotmail.com>
2021-04-25 09:33:04 +0200

    i did function to do increase the snake but it does not work

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -3,7 +3,9 @@ module Actions
         next_direction = state.curr_direction
         next_position = calc_next_position(state)
 
-        if position_is_valid?(state, next_position)
+        if position_is_food?(state, next_position)
+            grow_snake_to(state, next_position)
+        elsif position_is_valid?(state, next_position)
             move_snake_to(state, next_position)
         else
             end_game(state)
@@ -20,6 +22,16 @@ module Actions
     end
 
     private
+
+    def self.position_is_food?(state, next_position)
+        state.food.row == next_position.row && state.food.col == next_position.col
+    end
+
+    def self.grow_snake_to(state, next_position)
+        new_snake = [next_position] + state.snake
+        state.snake = new_snake
+        state
+    end
     
     def self.calc_next_position(state)
         new_position = state.snake.positions.first
